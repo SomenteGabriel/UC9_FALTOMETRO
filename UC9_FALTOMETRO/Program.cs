adfa7e52c25789e7a3827c6b3fb9a97de1b9def0
GABRIEL VIANNA CARVALHO DOS SANTOS <gabriel.vcsantos1@senacsp.edu.br>
2023-03-07 21:26:58 -0300

    att for

--- UC9_FALTOMETRO/Program.cs
+++ UC9_FALTOMETRO/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿int ucs = 0;
+int i = 0;
+int j = 0;
+string[,] curso = new string[ucs, 3];
+
+Console.WriteLine("Qual a quantidade de UCs?");
+ucs = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Agora vamos cadastrar!!\n\n\n\n                                                                Pressione ENTER para CONTINUAR.");
+for (i = 0; i<=ucs;i++)
+{
+    j = 0;
+    Console.WriteLine("Qual a UC?");
+    curso[i,j++]= Console.ReadLine();
+    Console.WriteLine("Descrição da UC?");
+    curso[i, j++] = Console.ReadLine();
+    Console.WriteLine("qual a carga horaria? (em horas)");
+    curso[i, j++] = Console.ReadLine();
+
+}
+
