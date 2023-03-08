218e1b52e280d61ce0dd01f8b82b5cf58f5aab07
GABRIEL VIANNA CARVALHO DOS SANTOS <gabriel.vcsantos1@senacsp.edu.br>
2023-03-07 22:05:08 -0300

    att pesquisa

--- UC9_FALTOMETRO/Program.cs
+++ UC9_FALTOMETRO/Program.cs
@@ -1,14 +1,20 @@
 ﻿int ucs = 0;
 int i = 0;
 int j = 0;
-string[,] curso = new string[ucs, 3];
+string[,] curso = new string[ucs, 4];
+string voltar = "s";
+double horas = 0;
+double dias = 0;
+
+
 
 Console.WriteLine("Qual a quantidade de UCs?");
 ucs = int.Parse(Console.ReadLine());
-
+int pesquisar = 0;
 Console.WriteLine("Agora vamos cadastrar!!\n\n\n\n                                                                Pressione ENTER para CONTINUAR.");
 for (i = 0; i<=ucs;i++)
 {
+    Console.Clear();
     j = 0;
     Console.WriteLine("Qual a UC?");
     curso[i,j++]= Console.ReadLine();
@@ -16,6 +22,33 @@ for (i = 0; i<=ucs;i++)
     curso[i, j++] = Console.ReadLine();
     Console.WriteLine("qual a carga horaria? (em horas)");
     curso[i, j++] = Console.ReadLine();
+    Console.WriteLine("qual a duração da sua aula diaria? (em horas)");
+    curso[i, j++] = Console.ReadLine();
 
 }
 
+while (voltar == "s" || voltar == "S")
+{
+    Console.Clear();
+    
+    Console.WriteLine("por qual UC deseja pesquisar?");
+    pesquisar = int.Parse(Console.ReadLine());
+
+    for (i = 0; i <= ucs; i++)
+    {
+        if (int.Parse(curso[i, 0]) == pesquisar)
+        {
+
+            Console.WriteLine("A UC: " + curso[i,0]+", "+curso[i,1]+", Com a carga horária de: "+curso[i,2]);
+
+            horas = ((double.Parse(curso[i, 2]) * 25 / 100));
+            dias = (double.Parse(curso[i, 2]) * 25 / 100) / double.Parse(curso[i, 3]);
+
+            Console.WriteLine("Você tem: "+horas.ToString("N0")+"h de oportunidades de falta.\nOque dá: "+dias.ToString("N0"));
+
+        }
+
+    }
+    Console.WriteLine("Deseja continuar pesquisando ? 'S' || 'N'. ");
+    voltar = Console.ReadLine();
+}
